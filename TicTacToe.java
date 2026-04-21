769c5a9502451a986721eeac2d367c0e65819c62
pranavsinhatutu-cell <pranavsinhatutu@gmail.com>
2026-04-21 15:13:12 +0530

    UC3

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,36 +1,63 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
 
-    // Entry point
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
+
+        int userSlot = getUserSlot();   
+        System.out.println("You selected slot: " + userSlot);
     }
 
-    // Initialize the board with '-'
-    static void initializeBoard() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = '-';
-            }
+    // UC2: Toss and assign symbols
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
     }
 
-    // Print the board
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int row = 0; row < 3; row++) {
-            System.out.print("| ");
-            for (int col = 0; col < 3; col++) {
-                System.out.print(board[row][col] + " | ");
+    // UC2: Display result
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human starts first.");
+        } else {
+            System.out.println("Computer starts first.");
+        }
+
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+    }
+
+    // UC3: Accept user slot input (1–9)
+    static int getUserSlot() {
+        int slot;
+
+        while (true) {
+            System.out.print("Enter a slot (1-9): ");
+            slot = sc.nextInt();
+
+            if (slot >= 1 && slot <= 9) {
+                return slot;
+            } else {
+                System.out.println("Invalid input! Enter number between 1 and 9.");
             }
-            System.out.println();
-            System.out.println("-------------");
         }
     }
 }
-
-            
-    
-
