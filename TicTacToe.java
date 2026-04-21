ec9eb7aab6e0fd4c12340a24d73b160fad216b34
pranavsinhatutu-cell <pranavsinhatutu@gmail.com>
2026-04-21 15:23:44 +0530

    UC4:Convert Slot Number (1–9) to Board Index (Row, Column)

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,51 +1,22 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
 
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
-
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        tossAndAssignSymbols();
-        displayTossResult();
 
-        int userSlot = getUserSlot();   
-        System.out.println("You selected slot: " + userSlot);
+        int slot = getUserSlot();   // take input
+
+        int row = getRowFromSlot(slot);
+        int col = getColFromSlot(slot);
+
+        System.out.println("You selected slot: " + slot);
+        System.out.println("Row: " + row);
+        System.out.println("Column: " + col);
     }
 
-    // UC2: Toss and assign symbols
-    static void tossAndAssignSymbols() {
-        Random rand = new Random();
-        int toss = rand.nextInt(2);
-
-        if (toss == 0) {
-            isHumanTurn = true;
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-        } else {
-            isHumanTurn = false;
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-
-    // UC2: Display result
-    static void displayTossResult() {
-        if (isHumanTurn) {
-            System.out.println("Human starts first.");
-        } else {
-            System.out.println("Computer starts first.");
-        }
-
-        System.out.println("Human Symbol: " + humanSymbol);
-        System.out.println("Computer Symbol: " + computerSymbol);
-    }
-
-    // UC3: Accept user slot input (1–9)
+    // UC3 reused: take input (1–9)
     static int getUserSlot() {
         int slot;
 
@@ -59,5 +30,15 @@ public class TicTacToe {
                 System.out.println("Invalid input! Enter number between 1 and 9.");
             }
         }
+    }
+
+    // UC4: Convert slot → row
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    // UC4: Convert slot → column
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
     }
 }
